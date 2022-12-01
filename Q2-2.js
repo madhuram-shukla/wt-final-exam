4fd455aeb14918f3a7ce93f948a23b64e95888d2
madhuram-shukla <119510424+madhuram-shukla@users.noreply.github.com>
2022-12-01 13:05:42 +0530

    Add files via upload

---
+++ Q2-2.js
@@ -0,0 +1,13 @@
+function inAssOrder(arr){
+    let second_index;
+    for(let first_index = 0; first_index < arr.length; first_index++){
+      second_index = first_index + 1;
+      if(arr[second_index] - arr[first_index] < 0) return false;
+    }
+    return true;
+}
+
+console.log(inAssOrder([1, 2, 4, 7, 19]));
+console.log(inAssOrder([1, 2, 3, 4, 5]));
+console.log(inAssOrder([1, 6, 10, 18, 2, 4, 20]));
+console.log(inAssOrder([9, 8, 7, 6, 5, 4, 3, 2, 1]));
---
+++ Q4-2.js
@@ -0,0 +1,10 @@
+
+const mySentence = "i love javascript";
+const words = mySentence.split(" ");
+
+for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+}
+
+words.join(" ");
+console.log(words.join(" "));
